916efaa2546908bb72a2064a85a7310b848e3c3b
sivasindhu812 <121868214+sivasindhu812@users.noreply.github.com>
2023-01-04 05:36:11 +0000

    Add files via upload

---
+++ 2div.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyFirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter the number:");
+            n = Int32.Parse(Console.ReadLine());
+
+            if ( n % 2 == 0)
+            {
+                Console.WriteLine($"{n} is divisible by 2");
+            }
+            else
+            {
+                Console.WriteLine($"{n} is not divisible by 2");
+            }
+            
+        }
+    }
+}
+
+
